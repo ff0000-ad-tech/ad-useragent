be940145017d3397adbac5b503365230fdb44ba7
Kenny Arehart <kenny.arehart@ff0000.com>
2019-04-08 09:33:12 -0700

    renamed type to device; updated to isDevice & getDevice for clarity

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import { agent } from './lib/agent.js'
-import { isType, getType } from './lib/type.js'
+import { isDevice, getDevice } from './lib/type.js'
 import { isBrowser, getBrowser, getBrowserVersion } from './lib/browser.js'
 import { getBrand, getProduct, getOS, getOSVersion, isOS } from './lib/brandAndOS.js'
 
-export { isType, getType, isBrowser, getBrowser, getBrowserVersion, getBrand, getProduct, getOS, getOSVersion, isOS }
+export {
+	isDevice,
+	getDevice,
+	isBrowser,
+	getBrowser,
+	getBrowserVersion,
+	getBrand,
+	getProduct,
+	getOS,
+	getOSVersion,
+	isOS
+}
 
 export const pixelRatio = window.devicePixelRatio || 'unknown'
 
@@ -22,7 +33,7 @@ export function report() {
 	console.log('ad-useragent > report()')
 
 	// TODO - update type in this exception
-	// if (!isType('desktop') && isOS('windows') && getOSVersion() <= 8 && isBrowser('ie') && getBrowserVersion < 12) {
+	// if (!isDevice('desktop') && isOS('windows') && getOSVersion() <= 8 && isBrowser('ie') && getBrowserVersion < 12) {
 	// 	console.log(
 	// 		"You appear to be on Windows 7 or 8 using Internet Explorer 11 or under. You also appear to be on a touchscreen device. We will assume you're actually on a desktop, since it's extremely unlikely you're on a tablet or mobile device using this specific operating system and browser."
 	// 	)
@@ -34,7 +45,7 @@ export function report() {
 	str += '\n AGENT:\n\n\t' + agent + '\n'
 	str += '\n  Brand:\t\t\t' + getBrand()
 	str += '\n  Product:\t\t\t' + getProduct()
-	str += '\n  Type:\t\t\t\t' + getType()
+	str += '\n  Type:\t\t\t\t' + getDevice()
 	str += '\n  Os:\t\t\t\t' + getOS() + ' - ' + getOSVersion()
 	str += '\n  Browser:\t\t\t' + getBrowser() + ' - ' + getBrowserVersion()
 	const d = getDimensions()
