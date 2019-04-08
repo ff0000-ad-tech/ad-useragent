7565427e7780556e5e03d8a04fbc55d48ab09113
Kenny Arehart <kenny.arehart@ff0000.com>
2019-04-08 13:51:41 -0700

    moved agent to index; updated docs across all files; removed docs.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { agent } from './lib/agent.js'
 import { isDevice, getDevice } from './lib/device.js'
 import { isBrowser, getBrowser, getBrowserVersion } from './lib/browser.js'
 import { getBrand, getProduct, getOS, getOSVersion, isOS } from './lib/brandAndOS.js'
@@ -16,36 +15,66 @@ export {
 	isOS
 }
 
+/**	
+	@memberOf ad-useragent	
+	@var {string} agentString
+	@desc
+		Current user agent of browser. 
+*/
+export const agentString = navigator.userAgent
+
+/**	
+	@memberOf ad-useragent	
+	@var {string} pixelRatio
+	@desc
+		Pixel ratio of device viewport. 
+*/
 export const pixelRatio = window.devicePixelRatio || 'unknown'
 
-export function getOrientation() {
+/**	
+	@memberOf ad-useragent	
+	@method getOrientation
+	@returns {string}
+	@desc
+		Orientaion of device viewport: landscape or portrait. 
+*/
+export const getOrientation = () => {
 	return window.innerWidth > window.innerHeight ? 'landscape' : 'portrait'
 }
 
-export function getDimensions() {
+/**	
+	@memberOf ad-useragent
+	@method getDimensions
+	@returns {object}	
+	@property {number} width
+		window inner-width
+	@property {number} height
+		window inner-height
+	@desc
+		The current dimensions of the device's viewport, returns an object with a width & height 
+		value that are direct returns of windowWidth and windowHeight.
+*/
+export const getDimensions = () => {
 	return {
 		width: window.innerWidth,
 		height: window.innerHeight
 	}
 }
 
-export function report() {
-	console.log('ad-useragent > report()')
-
-	// TODO - update type in this exception
-	// if (!isDevice('desktop') && isOS('windows') && getOSVersion() <= 8 && isBrowser('ie') && getBrowserVersion < 12) {
-	// 	console.log(
-	// 		"You appear to be on Windows 7 or 8 using Internet Explorer 11 or under. You also appear to be on a touchscreen device. We will assume you're actually on a desktop, since it's extremely unlikely you're on a tablet or mobile device using this specific operating system and browser."
-	// 	)
-	// 	D.type = 'desktop'
-	// }
-
+/**	
+	@memberOf ad-useragent	
+	@method report
+	@desc
+		Called from within the pipeline, logs out the useragent string & all available pasered data for 
+		brand, product, device, OS & version, browser & version, dimensions, orientation, & pixel ratio
+*/
+export const report = () => {
 	console.log(
 		Array(70).join('-'),
-		'\n AGENT:\n\n\t' + agent + '\n',
+		'\n useragent:\n\n\t' + agentString + '\n',
 		'\n  Brand:\t\t\t' + getBrand(),
 		'\n  Product:\t\t\t' + getProduct(),
-		'\n  Type:\t\t\t\t' + getDevice(),
+		'\n  Device:\t\t\t' + getDevice(),
 		'\n  Os:\t\t\t\t' + getOS() + ' - ' + getOSVersion(),
 		'\n  Browser:\t\t\t' + getBrowser() + ' - ' + getBrowserVersion(),
 		'\n  Dimensions: \t\t' + getDimensions().width + 'x' + getDimensions().height,
